e07f83d11e2a68c7c967030f738e649a26be0a3f
mariamkhmahran <mariamkhmahran@hotmail.com>
2026-04-18 00:14:47 +0200

    Extra confirmation assertion before destructive actions

--- app/agent.py
+++ app/agent.py
@@ -5,133 +5,9 @@ from .tools import add_note, search_notes, delete_note, update_note, fetch_all
 import json
 
 load_dotenv()
-TOOLS = [
-    {
-        "type": "function",
-        "name": "search_notes",
-        "description": "Search for notes by keyword or tags. Use this when the user refers to notes without specifying an ID.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "keyword": {
-                    "type": "string",
-                    "description": "The keyword text used in the SELECT statement",
-                },   
-                "tags": {
-                    "type": "array",
-                    "description": "A list of possible tags for the target note",
-                    "items": {
-                        "type": "string",
-                    }
-                },
-                "limit": {
-                    "type": "integer",
-                    "description": "Maximum number of notes to return.",
-                    "minimum": 1,
-                    "maximum": 100,
-                    "default": 20
-                },
-                "start_date": {
-                    "type": "string",
-                    "description": "Optional start of date range, inclusive. Format: 'YYYY-MM-DD' or 'YYYY-MM-DD'.",
-                    "nullable": True
-                    },
-                "end_date": {
-                    "type": "string",
-                    "description": "Optional end of date range, inclusive. Format: 'YYYY-MM-DD' or 'YYYY-MM-DD'.",
-                    "nullable": True
-                },
-            },
-            "required": ["query"],
-        },
-    },
-    {
-        "type": "function",
-        "name": "fetch_all",
-        "description": "Fetch all user notes from database",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "limit": {
-                    "type": "integer",
-                    "description": "Maximum number of notes to return.",
-                    "minimum": 1,
-                    "maximum": 1000,
-                    "nullable": True
-                },
-                "order": {
-                    "type": "string",
-                    "enum": ["ASC", "DESC"],
-                    "description": "Direction of list order. Controls whether to fetch newest or oldest notes first.",
-                    "nullable": True,
-                    "default": "ASC"
-                }
-            },
-        },
-    },
-    {
-        "type": "function",
-        "name": "add_note",
-        "description": "Add a new note with a title, body text and category tags.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "title": {
-                    "type": "string",
-                    "description": "The title of the note",
-                },
-                "body": {
-                    "type": "string",
-                    "description": "The body text of the note",
-                },
-                "tags": {
-                    "type": "array",
-                    "description": "A set of tags or categories for the note content",
-                    "items": {
-                        "type": "string"
-                    }
-                },
-            },
-            "required": ["title", "body"],
-        },
-    },
-    {
-        "type": "function",
-        "name": "update_note",
-        "description": "Update an existing note. Requires a note_id obtained from search_notes.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "note_id": {"type": "integer"},
-                "title": {"type": "string"},
-                "body": {"type": "string"},
-                "tags": {
-                    "type": "array",
-                    "items": {"type": "string"}
-                }
-            },
-            "required": ["note_id"]
-        }
-    },
-    {
-        "type": "function",
-        "name": "delete_note",
-        "description": "Delete a note by its ID. Only call this after confirming with the user.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "ids": {
-                    "type": "array",
-                    "description": "List of note ids to be deleted",
-                    "items": {
-                        "type": "string"
-                    }
-                }
-            },
-            "required": ["note_id"]
-        }
-    }
-]
+
+with open("app/tool_definitions.json", "r") as file:
+    TOOLS = json.load(file)["TOOLS"]
 
 class NotedAgent():
     def __init__(self):
@@ -172,17 +48,36 @@ class NotedAgent():
         agent_response = response.output[-1] 
         if agent_response != None and agent_response.type == "function_call":
             # If the agent responds with a tool call, excute the corresponding tool function
+            tool_name = agent_response.name
             args = json.loads(agent_response.arguments)
+
+            # If it belongs to dangerous actions, 
+            # check if the last user message contained a 'yes' or a confirmation keyword
+            if tool_name in ["delete_note", "update_note"]:
+                last_user_msg = next((m for m in reversed(self.chat_history) if "role" in m and m["role"] == "user"), {})
+                confirmation_words = ["yes", "do it", "confirm", "yep", "sure"]
+                print("=> last_user_msg", last_user_msg)
+                
+                user_confirmed = any(("content" in last_user_msg and word in last_user_msg["content"].lower()) for word in confirmation_words)
+                print("=> user_confirmed", user_confirmed)
+
+                if not user_confirmed:
+                    self.chat_history += [{
+                        "role": "system",
+                        "content": f"Please confirm the action with the user."
+                    }]
+                    return
             
-            if agent_response.name == "search_notes":
+            # excute tool call
+            if tool_name == "search_notes":
                 result = search_notes(**args)
-            elif agent_response.name == "add_note":
+            elif tool_name == "add_note":
                 result = add_note(**args)
-            elif agent_response.name == "update_note":
+            elif tool_name == "update_note":
                 result = update_note(**args)
-            elif agent_response.name == "delete_note":
+            elif tool_name == "delete_note":
                 result = delete_note(**args)
-            elif agent_response.name == "fetch_all":
+            elif tool_name == "fetch_all":
                 result = fetch_all(**args)
 
             self.chat_history += [
